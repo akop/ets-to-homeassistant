be16f558f3c06b197ae4befbff0a7228ec364f48
akop <andreas.koppenhoefer@stackit.cloud>
2024-12-07 10:14:23 +0100

    rename to run.sh / update gitignore

--- andi.sh
+++
@@ -1,1 +0,0 @@
-./ets_to_hass.rb --full-name --trace warn --lambda disable_custom.rb --format homeass Ets_Projekt/etstohasstest.knxproj > knx_ets2hass.yaml
---
+++ run.sh
@@ -0,0 +1,1 @@
+./ets_to_hass.rb --full-name --trace warn --lambda disable_custom.rb --format homeass input.knxproj > knx_ets2hass.yaml
